707f4618ec6ee698029478bc94393140a70331f7
Steffen Andreas Kloster <steffenakloster@gmail.com>
2023-06-12 11:47:21 +0200

    Update functions.php - tilføjet kommentarer

--- astra-child/functions.php
+++ astra-child/functions.php
@@ -22,19 +22,21 @@ function child_enqueue_styles() {
 
 }
 
+// Tilføj en handling, der kører når WordPress admin initialiseres
 add_action('admin_init', function () {
-    // Redirect any user trying to access comments page
+    // Omdiriger enhver bruger, der forsøger at få adgang til kommentarsiden
     global $pagenow;
     
+    // Hvis den nuværende side er edit-comments.php, omdiriger brugeren til admin dashboard
     if ($pagenow === 'edit-comments.php') {
         wp_safe_redirect(admin_url());
         exit;
     }
 
-    // Remove comments metabox from dashboard
+    // Fjern kommentarer metabox fra dashboardet
     remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
 
-    // Disable support for comments and trackbacks in post types
+    // Deaktiver understøttelse af kommentarer og trackbacks i indlægstyper
     foreach (get_post_types() as $post_type) {
         if (post_type_supports($post_type, 'comments')) {
             remove_post_type_support($post_type, 'comments');
@@ -43,23 +45,32 @@ add_action('admin_init', function () {
     }
 });
 
-// Close comments on the front-end
+// Luk kommentarer på front-end
+// Tilføj et filter, der returnerer falsk når WordPress tjekker om kommentarer er åbne
 add_filter('comments_open', '__return_false', 20, 2);
+// Tilføj et filter, der returnerer falsk når WordPress tjekker om pings er åbne
 add_filter('pings_open', '__return_false', 20, 2);
 
-// Hide existing comments
+// Skjul eksisterende kommentarer
+// Tilføj et filter, der returnerer et tomt array når WordPress henter kommentarer til en post
 add_filter('comments_array', '__return_empty_array', 10, 2);
 
-// Remove comments page in menu
+// Fjern kommentar side i menuen
+// Tilføj en handling, der fjerner kommentar siden fra admin menuen når admin menuen bygges
 add_action('admin_menu', function () {
     remove_menu_page('edit-comments.php');
 });
 
-// Remove comments links from admin bar
+// Fjern kommentar links fra admin baren
+// Tilføj en handling, der fjerner kommentar menuen fra admin baren når WordPress initialiseres
 add_action('init', function () {
+    // Tjekker først, om admin baren vises
     if (is_admin_bar_showing()) {
+        // Hvis den gør, fjernes kommentar menuen fra admin baren
         remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
     }
 });
 
+// Tilføj en handling, der kører når WordPress tilføjer scripts og styles til køen
+// Denne funktion tilføjer barn tema styles til køen efter forældre tema styles
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
